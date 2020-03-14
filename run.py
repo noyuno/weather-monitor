84b7b5d57aea796fdce4d71cd4c76319a15551b1
noyuno <noyuno@users.noreply.github.com>
2020-03-14 18:17:08 +0900

    aa

--- run.py
+++ run.py
@@ -9,6 +9,9 @@ import inky
 
 inky_display_WIDTH = 250
 inky_display_HEIGHT = 128
+DEFAULT_FONT = '/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc'
+font22 = ImageFont.truetype(DEFAULT_FONT, 22, encoding='unic')
+
 
 class InkyPHAT(inky.Inky):
     WIDTH = 250
@@ -22,5 +25,8 @@ class InkyPHAT(inky.Inky):
         inky.Inky.__init__(self, resolution=(self.WIDTH, self.HEIGHT), colour=colour, h_flip=False, v_flip=False) 
 
 inky_display = InkyPHAT('black')
-inky_display.drawString('こんにちは世界')
+img = Image.new("P", (inky_display_WIDTH, inky_display_HEIGHT))
+draw = ImageDraw.Draw(img)
+draw.text((0,0),text1,font=font22,fill=1)
+inky_display.set_image(img)
 inky_display.show()
